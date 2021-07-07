4e0bcbf0bdb18617a8a24be062762f4501712fc3
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-07 12:21:16 -0700

    only updating marcos.js

--- assets/js/marcos.js
+++ assets/js/marcos.js
@@ -1,43 +1,24 @@
-var responseText = document.getElementById('response-text');
+var responseCard = $('#response-text');
 
-
-function getIp(){
-
-    fetch("http://api.ipstack.com/check?access_key=891ca2fd6b3e3f2e296a2e3259012ed0",{
-        cache: 'reload',
-    })
-    .then(function (response) {
-        if (response.status === 200){
-            responseText.textContent =respsonse.stauts;
-        }
-        else if (response.status !==200){
-            responseText = "There was an error";
-        }
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data)
-        for (let i = 0; i < data.length; i++) 
-    })
-}
     
-function getBrewery(){
-    fetch ("https://api.openbrewerydb.org/breweries?per_page=25", {
-    cache: 'reload'
-    })
-    .then(function (response){
-    if (response.status === 200){
-        responseText.textContent = response.status;
-    }
-    else if (response.status !==200){
-        responseText = "there was an error";
-    }
-    return response.jspon();
-    })
+function getBreweryByLocation(lat , long){
+    let apiUrl = `https://api.openbrewerydb.org/breweries?by_dist=${lat},${long}&per_page=50`; 
+    fetch (apiUrl)
 
-    .then(function (data){
-    console.log(data);
-    for (let i = 0; i < data.length; i++) {
-         
+        .then(function (response){
+            if (response.status === 200){
+                response.json().then(function (data){
+                    //todo: choose random from this list
+                    console.log(data); 
+                })
+            }
+            else if (response.status !==200){
+            console.log (`there was an error: ${response.status}`);
+            }
+            else {
+                console.log("no breweries found at this location");
+            }
+        });
     }
-    })}
+
+
